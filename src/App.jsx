e9bd3f4edf1e3c95f4d6579b62e9f3c737ff8d82
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-05 12:35:47 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login
- from './pages/Login'
+import Login from './pages/Login'
+import AppointmentCard from './components/AppointmentCard'
 
 function App() {
 
   return (
     <>
       <Login />
+      <AppointmentCard />
     </>
   )
 }
