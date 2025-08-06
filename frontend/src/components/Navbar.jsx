925b1912bb4d252fe9591eaefedf0bf9536a881a
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-06 22:59:55 +0530

    navbar.jsx

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={Assets.logo} alt="" />
+      <ul>
+        <NavLink>
+            <li>Home</li>
+            <hr />
+        </NavLink>
+                <NavLink>
+            <li>All Doctors</li>
+            <hr />
+        </NavLink>
+                <NavLink>
+            <li>About</li>
+            <hr />
+        </NavLink>
+                <NavLink>
+            <li>Contact</li>
+            <hr />
+        </NavLink>
+    </ul>
+    <div>
+
+        <button>Create Account</button>
+
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
