a8aab5f20fa1c48142b70066f6b387c1afe526cb
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-07 00:49:13 +0530

    cc

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,26 +7,26 @@ const Navbar = () => {
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
       <img className='w-44 cursor-pointer' src={assets.MediHelpLogo} alt="" />
       <ul className='hidden md:flex items-start gap-5 font-medium'>
-        <NavLink>
+        <NavLink to="/">
             <li className='py-1'>Home</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
-                <NavLink>
+        <NavLink to="/doctors">
             <li className='py-1'>All Doctors</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
-                <NavLink>
+        <NavLink to="/about">
             <li className='py-1'>About</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
-                <NavLink>
+        <NavLink to="/contact">
             <li className='py-1'>Contact</li>
-            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
     </ul>
     <div>
 
-        <button>Create Account</button>
+        <button className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
 
     </div>
     </div>
