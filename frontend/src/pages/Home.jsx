ef37c0b26819bed98c435af8d766c4efce9a5263
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-06 23:00:31 +0530

    home

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to MediHelp Clinic Management System</h1>
+       
+      </div>
+    )
+  }
+}
+
+export default Home
