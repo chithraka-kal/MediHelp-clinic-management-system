934af075b660b3407fca14c6cffc98f60a1644a5
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-13 23:48:32 +0530

    top doctors

---
+++ frontend/src/components/TopDoctors.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
+
+
+const TopDoctors = () => {
+
+    const navigate = useNavigate();
+    const { doctors } = useContext(AppContext);
+
+  return (
+    <div className='flex flex-col items-center gap-4 my-16 text-gray-800 md:mx-10' id='top-doctors'>
+      <h1 className='text-3xl font-medium'>TopDoctors To See</h1>
+      <p className='sm:w-1/3 text-sm text-center'>Here are some of the top doctors you can consult</p>
+      <div className='w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+        {doctors.slice(0, 10).map((doctor, index) => (
+          <div onClick={() => navigate(`/appointments/${doctor._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+            <img className='bg-blue-50' src={doctor.image} alt={doctor.name}/>
+            <div className='p-4'>
+                <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                    <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                </div>
+                <p className='text-gray-900 text-lg font-medium'>{doctor.name}</p>
+              <p className='text-gray-600 text-sm'>{doctor.speciality}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button onClick={() => { navigate('/doctors'); scrollTo(0, 0); }} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>See All Doctors</button>
+    </div>
+  )
+}
+
+export default TopDoctors
