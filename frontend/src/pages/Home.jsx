273b70038ad088169193ed1be5c4e45d20b74cc7
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-13 12:32:58 +0530

    home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
+import Header from '../components/Header'
 
 export class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to MediHelp Clinic Management System</h1>
-       
+        <Header />
       </div>
     )
   }
