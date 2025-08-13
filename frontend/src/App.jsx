1f761f3cbfe3b5b5f1c9cc488bcc58d34ded1bd3
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-14 00:38:38 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import MyAppointments from './pages/MyAppointments'
 import MyProfile from './pages/MyProfile'
 import Appointments from './pages/Appointments'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
         <Route path="/my-appointments" element={<MyAppointments />} />
         <Route path="/appointments/:id" element={<Appointments />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
