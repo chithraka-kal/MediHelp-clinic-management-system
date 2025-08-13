886833d5c643749c9a68847696a0d6060068ab1d
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-14 00:38:55 +0530

    home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import Header from '../components/Header'
+import SpecialtyMenu from '../components/SpecialtyMenu'
+import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
 
 export class Home extends Component {
   render() {
     return (
       <div>
         <Header />
+        <SpecialtyMenu />
+        <TopDoctors />
+        <Banner />
       </div>
     )
   }
