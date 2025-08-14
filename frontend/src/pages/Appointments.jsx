e5a70e4a54a65bc4fccb9935602915a6ede4d6bd
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-14 19:15:03 +0530

    appointments

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
 
 const Appointments = () => {
+
+  const { docId } = useParams();
+  const { doctors } = useContext(AppContext);
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find(doc => doc._id == docId);
+    setDocInfo(docInfo);
+  }
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
+
   return (
+
     <div>
       
+      <div>
+        <div>
+          <img src={docInfo.image} alt="" />
+        </div>
+      </div>
+
     </div>
   )
 }
