aecead767936a434e664e34101099d8e45ae38ba
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-16 19:42:06 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="/my-profile" element={<MyProfile />} />
         <Route path="/my-appointments" element={<MyAppointments />} />
-        <Route path="/appointments/:id" element={<Appointments />} />
+        <Route path="/appointments/:docId" element={<Appointments />} />
       </Routes>
       <Footer />
     </div>
