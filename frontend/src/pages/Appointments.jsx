72c9672a8ff91f9e3f5109768d1dd9b176d90579
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-20 00:00:09 +0530

    doc info

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -22,20 +22,27 @@ const Appointments = () => {
 
     <div>
       
-      <div>
+      <div className='flex flex-col sm:flex-row gap-4'>
         <div>
-          <img src={docInfo?.image} alt="" />
+          <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo?.image} alt="" />
         </div>
-        <div>
-          <p>{docInfo?.name} <img src={assets.verified_icon} alt="" /></p>
-          
+        <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
+          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>{docInfo?.name}
+             <img className='w-5' src={assets.verified_icon} alt="" /></p>
+          <div className='flex items-center gap-2 text-sm text-gray-600 mt-1'>
           <p>{docInfo?.degree} - {docInfo?.speciality}</p>
-          <button>{docInfo?.experience}</button>
+          <button className='py-0.5 px-2 border text-xs border-gray-400 rounded-full'>{docInfo?.experience}</button>
+          </div>
           <div>
-            <p>About <img src={assets.info_icon} alt="" /></p>
-          <p>{docInfo?.about}</p>
+            <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>
+              About <img src={assets.info_icon} alt="" /></p>
+          <p className='text-sm text-gray-500 mx-w-[700px] mt-1'>
+            {docInfo?.about}</p>
           
           </div>
+          <p>
+            Appointment Fee: <span className='text-primary font-medium'>{docInfo?.fees}</span>
+          </p>
         </div>
       </div>
 
