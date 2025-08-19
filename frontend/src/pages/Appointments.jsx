991d602f125c250c6d3d69fd52b8205a77b7cf08
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-20 03:12:50 +0530

    appointment

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -6,17 +6,74 @@ import { assets } from '../assets/assets';
 const Appointments = () => {
 
   const { docId } = useParams();
-  const { doctors } = useContext(AppContext);
+  const { doctors, currencySymbol } = useContext(AppContext);
+  const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
   const [docInfo, setDocInfo] = useState(null);
+  const [docSlots, setDocSlots] = useState([]);
+  const [slotIndex, setSlotIndex] = useState(0);
+  const [slotTime, setSlotTime] = useState('');
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find(doc => doc._id === docId);
     setDocInfo(docInfo);
   }
 
+  const getAvailableSlots = async () => {
+    setDocSlots([])
+
+    let today = new Date();
+    
+    for (let i = 0; i < 7; i++) {
+      //getting next 7 days
+      let currentDate = new Date(today);
+      currentDate.setDate(today.getDate() + i);
+
+    //setting end time of the date with index
+      let endTime = new Date();
+      endTime.setDate(today.getDate() + i);
+      endTime.setHours(21, 0, 0, 0);
+
+    //setting hours
+    if (today.getDay() === currentDate.getDay()) {
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10);
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0);
+      }
+      else{
+        currentDate.setHours(10);
+        currentDate.setMinutes(0);
+      }
+
+      let timeSlots = [];
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+        //add slot to the array
+        timeSlots.push({
+          datetime: new Date(currentDate),
+          time: formattedTime,
+          
+        });
+
+        //increment by 30 minutes
+        currentDate.setMinutes(currentDate.getMinutes() + 30);
+      }
+
+      setDocSlots(prevSlots => [...prevSlots, timeSlots]);
+    }
+  }
+
   useEffect(() => {
     fetchDocInfo();
   }, [doctors, docId]);
+
+  useEffect(() => {
+    getAvailableSlots
+  }, [docInfo]);
+
+  useEffect(() => {
+    getAvailableSlots();
+  }, [docInfo]);
 
   return docInfo && (
 
@@ -40,10 +97,36 @@ const Appointments = () => {
             {docInfo?.about}</p>
           
           </div>
-          <p>
-            Appointment Fee: <span className='text-primary font-medium'>{docInfo?.fees}</span>
+          <p className='font-medium text-gray-500 mt-5'>
+            Appointment Fee: <span className='text-gray-600'>
+              {currencySymbol}{docInfo?.fees}</span>
           </p>
         </div>
+      </div> 
+
+      {/* Booking Slots */}
+      <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
+        <p>Booking Slots</p>
+        <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+          {
+            docSlots.length && docSlots.map((item, index) => (
+              <div onClick={() => setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' : 'border border-gray-200'}`} key={index}>
+                <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                <p>{item[0] && item[0].datetime.getDate()}</p>
+              </div>
+            ))
+          }
+        </div>
+        <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+          { docSlots.length && docSlots[slotIndex].map((item, index) => (
+            <p onClick={() => setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
+              {item.time.toLowerCase()}
+            </p>
+            ))}
+        </div>
+        <button className='bg-primary text-white text-sm px-14 py-3 rounded-full my-6 font-light' disabled={!slotTime}>
+          Book an Appointment
+        </button>
       </div>
 
     </div>
