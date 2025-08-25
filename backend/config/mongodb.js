d993ead210a7aaa20e4c8fd6e214f761780d06aa
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-25 13:47:52 +0530

    mongodbjs

---
+++ backend/config/mongodb.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+
+    mongoose.connection.on('Connected', () => console.log('Database Connected'))
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
+}
+
+export default connectDB;
