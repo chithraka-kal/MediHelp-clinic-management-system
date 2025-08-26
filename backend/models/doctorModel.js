8ce4150f7407f7e191d01481d02377fff38593a6
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-26 16:10:22 +0530

    doctor model

--- backend/models/doctorModel.js
+++ backend/models/doctorModel.js
@@ -1,19 +1,19 @@
  import mongoose, { Types } from "mongoose"; 
 
  const doctorSchema = new mongoose.Schema({
-    name: {Type: String, required: true},
-    email: {Type: String, required: true, unique: true},
-    password: {Type: String, required: true},
-    image: {Type: String, required: true},
-    speciality: {Type: String, required: true},
-    degree: {Type: String, required: true},
-    experience: {Type: String, required: true},
-    about: {Type: String, required: true},
-    available: {Type: Boolean, required: true},
-    fees: {Type: Number, required: true},
-    address: {Type: Object, required: true},
-    date: {Type: Number, required: true},
-    slots_booked: {Type: Object, default: {}}
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    image: {type: String, required: true},
+    speciality: {type: String, required: true},
+    degree: {type: String, required: true},
+    experience: {type: String, required: true},
+    about: {type: String, required: true},
+    available: {type: Boolean, required: true},
+    fees: {type: Number, required: true},
+    address: {type: Object, required: true},
+    date: {type: Number, required: true},
+    slots_booked: {type: Object, default: {}}
 
  },{minimize: false})
 
