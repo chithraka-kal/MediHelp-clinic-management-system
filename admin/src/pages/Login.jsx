755ba05fd6026cd73ba06cdd08ace47cb654a17a
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-28 22:14:16 +0530

    login

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -19,10 +19,12 @@ const Login = () => {
 
       if (state === 'Admin') {
 
-        const {data} = await axios.post(backendUrl + '/admin/login', { email, password })
+        const {data} = await axios.post(backendUrl + 'api/admin/login', { email, password })
         if (data.success) {
-          localStorage.setItem('aToken', data.aToken)
-          setAtoken(data.aToken)
+
+          console.log("data.token")
+          // localStorage.setItem('aToken', data.token)
+          // setAtoken(data.token)
           
           
         }
@@ -37,7 +39,7 @@ const Login = () => {
   }
 
   return (
-    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex item-center'>
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
       <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96  rounded-xl text-[#5e5e5e] text-sm shadow-lg '>
         <p className='text-2xl font-semibold m-auto' ><span className='text-blue-500'>{state} </span> Login</p>
         <div className='w-full'>
@@ -48,7 +50,7 @@ const Login = () => {
           <p>Password</p>
           <input onChange={(e) => setPassword(e.target.value)} value={password} className='border border-[#DADADA] p-2 rounded-md w-full mt-1' type="password" required />
         </div>
-        <button  className='bg-[#5f6FFF] text-white w-full py-2 rounded-md text-base'>Login</button>
+        <button className='bg-[#5f6FFF] text-white w-full py-2 rounded-md text-base'>Login</button>
         {
           state === 'Admin'
           ? <p>Doctor Login? <span className='cursor-pointer text-blue-500 underline' onClick={() => setState('Doctor')}>Click here</span></p>
