ba419b09434185409a19366a412781f9f3f7e1cb
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-08-30 23:15:19 +0530

    doctor list

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -3,7 +3,7 @@ import { AdminContext } from '../../context/AdminContext'
 
 const DoctorsList = () => {
 
-  const {doctors, aToken, getAllDoctors} = useContext(AdminContext)
+  const {doctors, aToken, getAllDoctors, changeAvailability} = useContext(AdminContext)
 
   useEffect(() => {
     if (aToken) {
@@ -22,7 +22,7 @@ const DoctorsList = () => {
                 <p className='text-natural-800 text-lg font-medium'>{item.name}</p>
                 <p className='text-zinc-600 text-sm'>{item.speciality }</p>
                 <div className='mt-2 flex items-center gap-1 text-sm'>
-                  <input type="checkbox" checked={item.available} />
+                  <input onChange={() => changeAvailability(item.id)} type="checkbox" checked={item.available} />
                   <p>{item.available ? 'Available' : 'Not Available'}</p>
                 </div>
               </div>
