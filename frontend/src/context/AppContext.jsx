32f4dd3a4b54b7a492f46ccfcad4d0fb466b19e3
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-01 23:05:32 +0530

    app context

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -10,9 +10,7 @@ const AppContextProvider = (props) => {
     const currencySymbol = '$';
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [doctors, setDoctors] = useState([])
-
-    const [token, setToken] = useState('')
-
+    const [token, setToken] = useState(localStorage.getItem('token') || false)
 
     const getDoctorsData = async () => {
         try {
