5e9d56280170e1537ae57830f1463a6870c23cea
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-02 00:36:29 +0530

    authUser

--- backend/middlewares/authUser.js
+++ backend/middlewares/authUser.js
@@ -3,19 +3,20 @@ import jwt from "jsonwebtoken";
 //user authentication middleware
 const authUser = async (req, res, next) => {
     try {
-        const {token} = req.headers
-        if(!token){
+        const { token } = req.headers;
+        if (!token) {
             return res.json({ success: false, message: "No Authorized Access login again" });
         }
-        const token_decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.body.userId = token_decoded.id
-        next()
 
+        const token_decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.userId = token_decoded.id;
+
+        next();
     } catch (error) {
-        console.log(error)
+        console.log(error);
         res.json({ success: false, message: error.message });
     }
-
 }
 
 export default authUser;
