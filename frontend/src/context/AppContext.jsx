5360073eb2de19daaba3c2794aaa8ea500f1d604
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-01 22:27:31 +0530

    app context

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -5,15 +5,14 @@ import { toast } from "react-toastify";
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
-    
+
+      
     const currencySymbol = '$';
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [doctors, setDoctors] = useState([])
 
-    const value = {
-        doctors,
-        currencySymbol
-    }
+    const [token, setToken] = useState('')
+
 
     const getDoctorsData = async () => {
         try {
@@ -29,6 +28,13 @@ const AppContextProvider = (props) => {
             console.log(error)
             toast.error(error.message)
         }
+    }
+
+        const value = {
+        doctors,
+        currencySymbol,
+        token, setToken,
+        backendUrl
     }
 
     useEffect(() => {
