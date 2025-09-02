830a702eb8e6d0f303b492b3332df86a6831e014
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-02 10:30:48 +0530

    MyProfile

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,23 +1,12 @@
-import React, { useState } from "react";
-import { assets } from "../assets/assets";
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
 const MyProfile = () => {
-  const [userData, setUserData] = useState({
-    name: "John Doe",
-    image: assets.profile_pic,
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-    address: {
-      line1: "123 Main St",
-      line2: "Apt 4B",
-      city: "New York",
-    },
-    gender: "Male",
-    dob: "1990-01-01",
-  });
+  
+  const {userData, setUserData} = useContext(AppContext)
 
   const [isEdit, setIsEdit] = useState(false);
-  return (
+  return userData && (
     <div className="max-w-lg flex flex-col gap-2 text-sm">
       <img className="w-36 rounded-full" src={userData.image} alt="" />
       {isEdit ? (
