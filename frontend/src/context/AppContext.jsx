e6279193180342fa809ae36bd1f3be2baf107de4
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-02 10:30:38 +0530

    appContext

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -11,6 +11,7 @@ const AppContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') || false)
+    const [userData, setUserData] = useState(false)
 
     const getDoctorsData = async () => {
         try {
@@ -28,16 +29,40 @@ const AppContextProvider = (props) => {
         }
     }
 
+    const loadUserProfileData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/user/get-profile', {headers: {token}})
+            if (data.success) {
+                setUserData(data.userData)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
         const value = {
         doctors,
         currencySymbol,
         token, setToken,
-        backendUrl
+        backendUrl,
+        userData, setUserData,
+        loadUserProfileData
     }
 
     useEffect(() => {
         getDoctorsData()
     }, [])
+
+    useEffect(() => {
+        if (token) {
+            loadUserProfileData()
+        }else{
+            setUserData(false)
+        }
+    }, [token])
 
     return (
         <AppContext.Provider value={value}>
