e2c9697d2f00a874d134796e7ccb521b720fcaaa
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-02 23:32:18 +0530

    MyAppointment

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -32,6 +32,22 @@ const MyAppointments = () => {
     }
   }
 
+  const cancelAppointment = async (appointmentId) => {
+    try {
+      const {data} = await axios.post(backendUrl + '/api/user/cancel-appointment', {appointmentId}, {headers: {token}})
+      if (data.success) {
+        toast.success(data.message)
+        getUserAppointments()
+      }else{
+        toast.error(data.message)
+      }
+      
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
   useEffect(() => {
     if (token) {
       getUserAppointments()
@@ -56,7 +72,7 @@ const MyAppointments = () => {
             <div></div>
             <div className='flex flex-col gap-2 justify-end'>
               <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-blue-500 hover:text-white transition-all duration-300'>Pay Online</button>
-              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-500 hover:text-white transition-all duration-300'>Cancel Appointment</button>
+              <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-500 hover:text-white transition-all duration-300'>Cancel Appointment</button>
             </div>
           </div>
         ))}
