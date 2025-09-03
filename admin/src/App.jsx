f3c69ff647cc760ec233206bd4f2779db8200c4e
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-04 03:14:10 +0530

    app

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Admin/Dashboard';
-import AllApointments from './pages/Admin/AllApointments';
+import AllAppointments from './pages/Admin/AllAppointments';
 import AddDoctor from './pages/Admin/AddDoctor';
 import DoctorsList from './pages/Admin/DoctorsList';
 
@@ -26,7 +26,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<></>} />
           <Route path="/admin-dashboard" element={<Dashboard />} />
-          <Route path="/all-appointments" element={<AllApointments />} />
+          <Route path="/all-appointments" element={<AllAppointments />} />
           <Route path="/add-doctor" element={<AddDoctor />} />
           <Route path="/doctor-list" element={<DoctorsList />} />
         </Routes>
