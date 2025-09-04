86d745699044ad85ad20f49ecc65903566f8a0c8
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-04 11:47:10 +0530

    all appointment

--- admin/src/pages/Admin/AllAppointments.jsx
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -7,7 +7,7 @@ import { assets } from '../../assets/assets'
 
 const AllAppointments = () => {
 
-  const {aToken, appointments, getAllAppointments} = useContext(AdminContext)
+  const {aToken, appointments, getAllAppointments, cancelAppointment} = useContext(AdminContext)
   const {calculateAge, slotDateFormat} = useContext(AppContext)
 
   useEffect(() => {
@@ -44,9 +44,10 @@ const AllAppointments = () => {
               {
                 item.cancelled
                 ? <p className='text-red-400 text-xs font-medium'>Cancelled</p>
-                :<img className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
+                :<img onClick={() => cancelAppointment(item._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
 
               }
+              
             </div>
             
           ))
