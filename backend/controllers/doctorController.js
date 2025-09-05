54e4530080a657ada248e8df0f5e71c7f25f9295
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-05 10:03:47 +0530

    doctor controller

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,4 +1,6 @@
 import doctorModel from "../models/doctorModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 
 const changeAvailability = async (req, res) => {
@@ -24,5 +26,26 @@ const doctorList = async (req, res) => {
     }
 }
 
+//API for doctor login
+const loginDoctor = async (req, res) => {
+    try {
+        const {email, password} = req.body
+        const doctor = await doctorModel.findOne({email})
+        if(!doctor){
+            return res.json({success: false, message: "Doctor not found bro"})
+        }
+        const isMatch = await bcrypt.compare(password, doctor.password)
+        if (isMatch) {
+            const token = jwt.sign({id: doctor._id}, process.env.JWT_SECRET)
+            res.json({success:true,token})
+        }else{
+            return res.json({success: false, message: "Doctor not found yko"})
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+}
 
-export {changeAvailability, doctorList}
+
+export {changeAvailability, doctorList, loginDoctor}
