bb05e6779c73270ca0bc21ec5c0eef8cb587dbab
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-07 12:42:30 +0530

    doctor appointment

--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useContext } from 'react'
+import { DoctorContext } from '../../context/DoctorContext'
+import { useEffect } from 'react'
 
 const DoctorAppointments = () => {
+  const {dToken, appointments, getAppointments} = useContext(DoctorContext)
+
+  useEffect(()=>{
+    if (dToken) {
+      getAppointments()
+    }
+  },[dToken])
   return (
     <div>
       
