79739fb7934b4f3ceb47cc6209894bc4bf798cfd
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-08 01:59:42 +0530

    doctor context

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -11,6 +11,7 @@ const DoctorContextProvider = (props) => {
     const [dToken, setDToken] = useState(localStorage.getItem('dToken')? localStorage.getItem('dToken') : '')
     const [appointments, setAppointments] = useState([])
     const [dashData, setDashData] = useState(false)
+    const [profileData, setProfileData] = useState(false)
 
     const getAppointments = async () => {
         try {
@@ -72,6 +73,24 @@ const DoctorContextProvider = (props) => {
         }
     }
 
+    const getProfileData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/doctor/profile', {headers:{dToken}})
+            if (data.success) {
+                setProfileData(data.profileData)
+                console.log(data.profileData)
+                
+            }   else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error);            
+            toast.error(error.message)
+        }
+    }
+            
+            
+
     const value = {
         backendUrl,
         dToken,
@@ -79,7 +98,8 @@ const DoctorContextProvider = (props) => {
         appointments,
         setAppointments, getAppointments,
         completeAppointment, cancelAppointment,
-        dashData, setDashData, getDashData
+        dashData, setDashData, getDashData,
+        profileData, setProfileData, getProfileData
     }
 
     return(
