8dbf8d4904f275ad68e5f6814c3792bb55efed24
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-07 16:19:43 +0530

    doctor appointment

--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import { useContext } from 'react'
 import { DoctorContext } from '../../context/DoctorContext'
 import { useEffect } from 'react'
+import { AppContext } from '../../context/AppContext'
+import { assets } from '../../assets/assets'
 
 const DoctorAppointments = () => {
-  const {dToken, appointments, getAppointments} = useContext(DoctorContext)
+  const {dToken, appointments, getAppointments, cancelAppointment, completeAppointment} = useContext(DoctorContext)
+  const {calculateAge, slotDateFormat} = useContext(AppContext)
 
   useEffect(()=>{
     if (dToken) {
@@ -12,8 +15,41 @@ const DoctorAppointments = () => {
     }
   },[dToken])
   return (
-    <div>
-      
+    <div className='w-full max-w-6xl m-5'>
+      <p className='mb-3 text-lg font-medium'>All Appointment</p>
+      <div className='bg-white border rounded text-sm max-h-[80vh] min-h-[60vh] overflow-y-scroll'>
+        <div className='hidden sm:grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] grid-flow-col gap-1 py-3 px-6 border-b'>
+          <p>#</p>
+          <p>Patient</p>
+          <p>Payment</p>
+          <p>Age</p>
+          <p>Date $ Time</p>
+          <p>Fees</p>
+          <p>Action</p>
+        </div>
+        {
+          appointments.map((item, index) => (
+            <div key={index} className='flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid sm:grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50'>
+              <p className='max-sm:hidden'>{index + 1}</p>
+              <div className='flex item-center gap-2'>
+                <img  className='w-8 rounded-full' src={item.userData.image} alt="" /><p>{item.userData.name}</p>
+              </div>
+              <div>
+                <p className='text-xs inline border border-blue-500 px-2 rounded-full'>
+                  {item.paid ? 'Paid' : 'Not Paid'}
+                </p>
+              </div>
+              <p className='max-sm:hidden'>{calculateAge(item.userData.dob)}</p>
+              <p>{slotDateFormat(item.slotDate)} {item.slotTime}</p>
+              <p>${item.docData.fees}</p>
+              <div className='flex'>
+                <img onClick={() => cancelAppointment(item._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
+                <img onClick={() => completeAppointment(item._id)} className='w-10 cursor-pointer' src={assets.tick_icon} alt="" />
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
