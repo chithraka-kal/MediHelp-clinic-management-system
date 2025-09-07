39364562eaf0bf299d54081621d409c703c9083e
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-07 22:03:12 +0530

    doctor appointment

--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -28,7 +28,7 @@ const DoctorAppointments = () => {
           <p>Action</p>
         </div>
         {
-          appointments.map((item, index) => (
+          appointments.reverse().map((item, index) => (
             <div key={index} className='flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid sm:grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50'>
               <p className='max-sm:hidden'>{index + 1}</p>
               <div className='flex item-center gap-2'>
@@ -42,10 +42,17 @@ const DoctorAppointments = () => {
               <p className='max-sm:hidden'>{calculateAge(item.userData.dob)}</p>
               <p>{slotDateFormat(item.slotDate)} {item.slotTime}</p>
               <p>${item.docData.fees}</p>
-              <div className='flex'>
+              {
+                item.cancelled
+                ? <p className='text-red-400 text-xs font-medium'>Cancelled</p>
+                : item.isCompleted
+                  ? <p className='text-green-400 text-xs font-medium'>Completed</p>
+                  : <div className='flex'>
                 <img onClick={() => cancelAppointment(item._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
                 <img onClick={() => completeAppointment(item._id)} className='w-10 cursor-pointer' src={assets.tick_icon} alt="" />
               </div>
+              }
+              
             </div>
           ))
         }
