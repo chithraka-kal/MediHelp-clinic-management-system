b6a8e218a92d95892a1b4204cf653d078fb61fec
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-09-08 16:15:40 +0530

    frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,23 +3,23 @@ import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div className='flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20'>
+    <div className='flex flex-col md:flex-row flex-wrap rounded-lg px-6 md:px-10 lg:px-20 bg-cover bg-center bg-no-repeat' style={{backgroundImage: `url(${assets.header_bg})`}}>
      {/* left side */}
     <div className='md:w-1/2 flex flex-col justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
-      <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
+      <p className='text-3xl md:text-4xl lg:text-5xl text-blue-800  rounded font-semibold leading-tight md:leading-tight lg:leading-tight'>
         Book Appointment <br/> With Trusted Doctors
       </p>
       <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
         <img className='w-28' src={assets.group_profiles} alt="" />
         <p>Simply browse through our extensive list of trusted doctors, <br className='hidden sm:block'/> schedule your appointment hassle-free.</p>
       </div>
-      <a href="#specialty" className='bg-white text-gray-600 px-8 py-3 rounded-full font-light flex items-center gap-2 w-fit hover:bg-gray-200 transition-all duration-300'>
-      Book Appointment <img className='w-3' src={assets.arrow_icon} alt="" />
+      <a href="#specialty" className='bg-white text-gray-600 px-8 py-3 rounded-full font-light flex items-center gap-2 w-fit hover:bg-blue-500 hover:text-white transition-all duration-300'>
+      Book Appointment <img className='w-3 ' src={assets.arrow_icon} alt="" />
       </a>
     </div>
     {/* right side */}
     <div className='md:w-1/2 relative'>
-    <img className='w-full md:absolute bottom-0 h-auto rounded-lg' src={assets.header_img} alt="" />
+    
     </div>
     </div>
   )
